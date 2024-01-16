94c8815484ce91dbb538b3e0e2207500e96aacff
RohitKumarSinha <sinharohit3333@gmail.com>
2024-01-16 15:15:25 +0530

    add: code of binary search

---
+++ debug.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        non_zero_index = 0
+
+        for i in range(len(nums)):
+            if (nums[i] != 0):
+                nums[non_zero_index], nums[i] = nums[i], nums[non_zero_index]
+                non_zero_index += 1
+
+        return nums
+
+# Example usage:
+solution = Solution()
+print(solution.moveZeroes([0,1,0,3,12, 20, 10, 0, 5]))
