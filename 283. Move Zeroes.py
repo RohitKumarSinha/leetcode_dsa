67b19bee5ee389bddca6dd899ab5a2dc42aaec94
RohitKumarSinha <sinharohit3333@gmail.com>
2024-01-16 17:08:39 +0530

    add code of shifting zeros

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        non_zero_index = 0
+
+        # Move non-zero elements to the beginning of the array
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[non_zero_index], nums[i] = nums[i], nums[non_zero_index]
+                non_zero_index += 1
+
+            
